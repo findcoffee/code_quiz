65d4e372f53e32a831f44c36d53b6b5b2a0bdcbf
sjpark <sjpark@unison.co.kr>
2020-09-02 12:53:00 +0900

    - 함수 형태로 테스트 까지 추가 변경

--- src/2920_scale/scale.py
+++ src/2920_scale/scale.py
@@ -1,19 +1,28 @@
 # https: // www.acmicpc.net/problem/2920
 
-a = list(map(int, input().split(' ')))
+def scale(arg):
+    a = list(map(int, arg.split(' ')))
 
-asc = True
-desc = True
+    asc = True
+    desc = True
 
-for i in range(1, 8):
-    if a[i] > a[i - 1]:
-        desc = False
-    elif a[i] < a[i - 1]:
-        asc = False
+    for i in range(1, 8):
+        if a[i] > a[i - 1]:
+            desc = False
+        elif a[i] < a[i - 1]:
+            asc = False
 
-if asc:
-    print('asc')
-elif desc:
-    print('desc')
-else:
-    print('mix')
+    if asc:
+        print('asc')
+    elif desc:
+        print('desc')
+    else:
+        print('mix')
+
+
+scale('1 2 3 4 5 6 7 8')
+scale('1 2 3 4 5 6 8 7')
+scale('8 7 6 5 4 3 2 1')
+scale('8 7 6 5 4 3 1 2')
+scale('3 2 1 4 5 6 7 8')
+scale('8 2 3 4 5 6 7 1')
